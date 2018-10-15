4a4877fe7e86959fedc74dde5299b6480b419c31
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2018-10-15 18:41:13 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* Changing active states */
+$(".navbar-nav .nav-link").hover(
+    function () {
+        $(".navbar .nav-link").removeClass("active");
+
+        $(this).addClass("active");
+    }
+);
+
+$(".log-out .fund-wallet").hover(
+    function () {
+        $(this).addClass("ewallet");
+    }
+);
+
+$(".log-out .fund-wallet").hover(
+    function () {
+        $(this).removeClass("ewallet");
+    }
+);
